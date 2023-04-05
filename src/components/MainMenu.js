411dfb0fdf745591211ef1b1eeb7d2be0cec6698
come0th3r <98780286+come0th3r@users.noreply.github.com>
2023-04-05 06:30:10 +0300

    - simple reDesign - shittyflutted colors - awful grid

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
-import {Box, Button, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+import {
+    AppBar,
+    Toolbar,
+    Box,
+    Button,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    Grid
+} from "@mui/material";
 import Leaderboard from "./Leaderboard";
 import {Typography} from "@mui/material";
+import "../styles.css";
+
 
 const MainMenu = ({ onStartQuiz, onLeaderboard }) => {
     const [selectedCategory, setSelectedCategory] = useState(null);
@@ -21,33 +33,50 @@ const MainMenu = ({ onStartQuiz, onLeaderboard }) => {
     };
 
     return (
-        <Box sx={{minWidth: 120, p: 2}} container align="center" justify="center" height="100vh">
-            <div sx={{p: 10}}>
-                <Typography variant="h1">Medical-quiz</Typography>
-                <FormControl variant="standard" sx={{ m: 1, minWidth: 120, width: 100 }}>
-                    <InputLabel htmlFor="category-select-label">Тема теста:</InputLabel>
-                    <Select
-                        labelID="category-select-label"
-                        id="category-select"
-                        value={selectedCategory}
-                        onChange={(e) => setSelectedCategory(e.target.value)}
-                    >
-                        <MenuItem value="Тема теста:"></MenuItem>
-                        {categories.map((category) => (
-                            <MenuItem value={category.value}>
-                                <em>{category.label}</em>
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-        </div>
-        <div>
-            <Button onClick={startQuiz}>Начать тест</Button>
-            <Button onClick={onLeaderboard}>Рейтинг игроков</Button>
-            <Leaderboard></Leaderboard>
-        </div>
+        <>
+        <Box sx={{minWidth: 120, p: 2, background:'#f7f5ff'}} container height="100vh">
+            <AppBar position="static" sx={{ borderRadius: "25px", background:'#f7f5ff', padding: 1}}>
+                <Toolbar className="header">
+                    <Grid container spacing={2}>
+                        <Grid item xs={8} edge="start" justify="flex-start">
+                            <Typography className="logoText" sx={{paddingLeft: 2, fontWeight: 700}} align="left" variant="h5">MQ</Typography>
+                        </Grid>
+                        <Grid display="flex" item xs={4} justifyContent="flex-end" style={{paddingRight: 2}}>
+                            <Button color="inherit">О Нас</Button>
+                            <Button color="inherit">Связаться</Button>
+                            <Button color="inherit">Помощь</Button>
+                    </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+            <Grid container style={{height:"80vh", alignItems: "center"}}>
+                <Grid item xs={6} style={{paddingLeft: 100}}>
+                    <Typography variant="h2" style={{textAlign:"left"}}>
+                        Пройди тест
+                        уже сейчас
+                    </Typography>
+                    <Typography variant="subtitle1" style={{color: "#808080", marginBottom: "1%"}}>
+                        Это абсолютно бесплатно!
+                    </Typography>
+                    <FormControl variant="outlined" sx={{minWidth: 300}}>
+                        <InputLabel htmlFor="category-select-label">Тема теста:</InputLabel>
+                        <Select labelID="category-select-label" id="category-select" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+                            <MenuItem value="Тема теста:"></MenuItem>
+                            {categories.map((category) => (
+                                <MenuItem value={category.value}>
+                                    <em>{category.label}</em>
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <Button variant='contained' onClick={startQuiz} style={{paddingBottom: 15, paddingTop: 15, marginLeft: 15, background:'#e23a1e'}}>Начать тест</Button>
+                </Grid>
+                <Grid item xs={6}>
+                    <img src="hero_girl.png" alt={"Привет!"} width="1024" style={{margin: -100}}/>
+                </Grid>
+            </Grid>
         </Box>
-
+        </>
     );
 };
 
